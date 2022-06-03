cb142666d87fe013acd2b1eb1ae5974816e7e394
elnurxo <elnurykh@code.edu.az>
2022-06-03 10:33:14 +0400

    1 june practise

---
+++ 1JunePractiseP227/1JunePractiseP227/Citizen.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1JunePractiseP227
+{
+    class Citizen
+    {
+        private string _name;
+        private string _surname;
+        public byte Age;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            } 
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value) && Char.IsUpper(value[0]))
+                {
+                    bool check1 = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (Char.IsDigit(value[i]))
+                        {
+                            check1 = true;
+                            break;
+                        }            
+                    }
+                    if (check1 == false)
+                    {
+                        _name = value;
+                    }
+                }
+              
+
+
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {     
+                if (!String.IsNullOrWhiteSpace(value) && Char.IsUpper(value[0]))
+                {
+                    bool check = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (Char.IsDigit(value[i]))
+                        {
+                            check = true;
+                            break;
+                        }       
+                    }
+                    if (check == false)
+                    {
+                        _surname = value;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 1JunePractiseP227/1JunePractiseP227/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1JunePractiseP227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen citizen1 = new Citizen()
+            {
+                Name = null,
+                Surname = "Khalilov1"
+            };
+            Console.WriteLine($"{citizen1.Name} {citizen1.Surname}");
+        }
+    }
+}
